e6911f6135930066f60bfb87434b19bc3283939e
BGW Group <89062325+bgwgroup@users.noreply.github.com>
2023-05-09 10:16:22 +1000

    Create heroSlider.js

---
+++ json/heroSlider.js
@@ -0,0 +1,18 @@
+[
+   {
+      "title":"10 Years of Work Hard, Play Hard!",
+      "subtitle":"Feb - May 2023",
+      "imgDesktop":"/_ui/responsive/common/images/cnw/home/hero-slider/01.png",
+      "imgMobile":"/_ui/responsive/common/images/cnw/home/hero-slider/01.png",
+      "btnText":"Learn more",
+      "btnLink":"#"
+   },
+  {
+      "title":"Bonus Prepaid VISA Gift Card",
+      "subtitle":"1 Feb - 28 Mar 2023",
+      "imgDesktop":"/_ui/responsive/common/images/cnw/home/hero-slider/02.jpg",
+      "imgMobile":"/_ui/responsive/common/images/cnw/home/hero-slider/02.jpg",
+      "btnText":"Learn more",
+      "btnLink":"#"
+   }
+]
